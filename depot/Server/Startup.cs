6f24002d18a3f703f89f040385296871685365d0
dahln <dahln@outlook.com>
2020-08-29 10:52:57 -0600

    Adjusting ForwardedHeaders

--- depot/Server/Startup.cs
+++ depot/Server/Startup.cs
@@ -50,12 +50,19 @@ namespace depot.Server
             services.Configure<AuthMessageSenderOptions>(Configuration);
 
             services.AddControllersWithViews();
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -73,11 +80,6 @@ namespace depot.Server
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
 
             app.UseIdentityServer();
             app.UseAuthentication();
